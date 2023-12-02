1e33c37b28435fe0bf7cc840cc636ceeec321552
Damian Kokot <73639471+damiankokot45@users.noreply.github.com>
2023-12-02 17:13:13 +0100

    02/12 t2

--- app.js
+++ app.js
@@ -1,5 +1,56 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   let squares = Array.from(document.querySelectorAll(".grid div"));
+  const scoredisplay = document.querySelector("#score");
+  const startbutt = document.querySelector("#start-button");
   const widht = 10;
+
+  //Elementy
+  const lTetromino = [
+    [1, widht + 1, widht * 2 + 1, 2],
+    [widht, widht + 1, widht + 2, widht * 2 + 2],
+    [1, widht + 1, widht * 2 + 1, widht * 2],
+    [widht, widht * 2, widht * 2 + 1, widht * 2 + 2],
+  ];
+
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+  ];
+
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1],
+  ];
+
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+  ];
+
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+  ];
+
+  const theTetrominoes = [
+    lTetromino,
+    zTetromino,
+    tTetromino,
+    oTetromino,
+    iTetromino,
+  ];
+
+  let currentPosition = 4;
+  let currentRotation = 0;
+
+  console.log(theTetrominoes[0][0]);
 });
